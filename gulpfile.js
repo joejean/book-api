a082f1186fdbe18b2bc7593ce125ef6f93abeb5e
Joe Jean <joeclef@hotmail.com>
2016-06-26 00:24:05 +0400

    refactoring and tests added

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp = require('gulp')
-	nodemon = require('nodemon');
+var gulp = require('gulp'),
+	nodemon = require('nodemon'),
+	gulpMocha = require('gulp-mocha');
 
 gulp.task('default', function(){
 	nodemon({
@@ -13,4 +14,9 @@ gulp.task('default', function(){
 	.on('restart',function(){
 		console.log('Restarted');
 	});
+});
+
+gulp.task('test', function(){
+	gulp.src('tests/*.js', {read: false})
+		.pipe(gulpMocha({reporter:'nyan'})); 
 });
